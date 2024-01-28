a4c38d02d73625fd70d936f93a9b277005cede52
Lucas Fernando <lucasfernando.dev@gmail.com>
2024-01-28 03:29:28 -0300

    add dynamic queue struture using c++

---
+++ estrutura-de-dados/fila dinamica/c++/filadinamica.h
@@ -0,0 +1,24 @@
+// Fila dinâmica = Dynamic Queue
+
+typedef int TipoItem;
+
+struct No{
+  TipoItem valor
+  No* proximo
+};
+
+class filadinamica{
+  private:
+  No* primeiro;
+  No* ultimo;
+
+
+  public:
+  filadinamica();
+  ~filadinamica();
+  bool estavazio();
+  bool estacheio();
+  void inserir(TipoItem item);
+  TipoItem remover();
+  void imprimir();
+};
